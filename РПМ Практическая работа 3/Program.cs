c1f6fb82a5dc6cf283bc691dd7d1813d29a3f92a
liza <123323177+younggengaar@users.noreply.github.com>
2024-03-13 16:14:19 +0200

    Add files via upload

---
+++ РПМ Практическая работа 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace РПМ_Практическая_работа_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = 5;
+            long result = CalculateFactorial(n);
+            Console.WriteLine($"Факториал числа {n} равен {result}");
+            Console.ReadKey();
+        }
+
+        static long CalculateFactorial(int number)
+        {
+            // Базовый случай: факториал 0 и 1 равен 1
+            if (number == 0 || number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                // Рекурсивный случай: n! = n * (n-1)!
+                return number * CalculateFactorial(number - 1);
+            }
+        }
+    }
+}
